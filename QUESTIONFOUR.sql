c376ea34306f51211d0a5c08c8922c5cd71583dc
Edit22d <nabwireedith18@gmail.com>
2025-05-21 21:31:51 +0400

    QUESTIONFOUR.sql

--- QUESTIONFOUR.sql
+++ QUESTIONFOUR.sql
@@ -1,73 +1,43 @@
--- MySQL dump 10.13  Distrib 5.6.22, for osx10.8 (x86_64)
---
--- Host: 127.0.0.1    Database: students
--- ------------------------------------------------------
--- Server version	5.7.18
+CREATE DATABASE Record;
+USE Record;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE TABLE student(
+code VARCHAR(50),
+stud_id INT NOT NULL PRIMARY KEY,
+Nname VARCHAR(255)
+);
 
---
--- Table structure for table `Course`
---
+CREATE TABLE course(
+code VARCHAR(50),
+Title VARCHAR(354)
+   
+);
 
-DROP TABLE IF EXISTS `Course`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Course` (
-  `code` varchar(24) DEFAULT NULL,
-  `Title` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO student(code,stud_id,Nname)
+VALUES('IMIS',001,'Damaris'),('BIT',002,'Okal'),('BIT',003,'Kahurio'),('CIT',004,'Mwangi');
 
---
--- Dumping data for table `Course`
---
+INSERT INTO course(code,Title)
+VALUES('IMIS','Info.system'),('BIT','BBachelor of IT'),('CIY','Cen in IT'),('DIT','Dip in IT');
 
-LOCK TABLES `Course` WRITE;
-/*!40000 ALTER TABLE `Course` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Course` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `tblstudents`
---
+-- i) Select all data from both tables
+SELECT * FROM student;
+SELECT * FROM course;
 
-DROP TABLE IF EXISTS `tblstudents`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tblstudents` (
-  `code` varchar(234) DEFAULT NULL,
-  `stud_id` int(11) NOT NULL,
-  `Name` varchar(23) DEFAULT NULL,
-  PRIMARY KEY (`stud_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- ii) Insert data into the student table
+INSERT INTO student(code,stud_id,Nname)
+VALUES('DIT',005,'Achieng');
 
---
--- Dumping data for table `tblstudents`
---
+-- iii) update data on aspecific student
+UPDATE student
+SET Nname = 'achieng odhiambo'
+WHERE stud_id = 005
 
-LOCK TABLES `tblstudents` WRITE;
-/*!40000 ALTER TABLE `tblstudents` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tblstudents` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+-- iv) deleting a specific course from the Course table
+DELETE FROM Course
+WHERE Code = 'DIT';
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- v)Order students by name in ascending order
+SELECT * FROM Student
+ORDER BY Name ASC;
 
--- Dump completed on 2025-05-19 13:34:00
